718e9b1905b8ef238cb9c7bc44031423af036b0b
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-08 18:01:08 -1000

    added if statement to check and see if key being passed is a valid key name

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
-  
+
+  var vault = {};
+  var keyName = [];
+  var keyValue = [];
+
+  vault.getValue = function( key ){
+    if(!key){
+      return null;
+    } else {
+      var keyIndex = keyName.indexOf(key);
+      if(keyIndex < 0){
+        return null;
+      } else {
+        return keyValue(keyIndex);
+      }
+    }
+  };
+
+  vault.setValue = function( key, value ) {
+    return null;
+  };
+
+return vault;
 };
